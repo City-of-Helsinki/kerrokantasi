d0de4fa00f7cc839579fe1dce6542a0e1f1e0897
piotras <piotrek.pokora@gmail.com>
2015-10-30 09:34:45 +0100

    Added simple serializer to avoid server error

--- kk/views/hearing.py
+++ kk/views/hearing.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
+from rest_framework import serializers
 
 from kk.models import Hearing
+
+class HearingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hearing
 
 class HearingViewSet(viewsets.ModelViewSet):
     """
     API endpoint for hearings.
     """
     queryset = Hearing.objects.all()
+    serializer_class = HearingSerializer

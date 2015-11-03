116117281e6cb840fffda44b99e3e448de4a0c4c
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-11-03 14:45:58 +0200

    Make hearing tests pass irrespective of database

--- kk/tests/test_hearing.py
+++ kk/tests/test_hearing.py
@@ -2,40 +2,40 @@ import pytest
 import datetime
 import urllib
 
+from django.utils.dateparse import parse_datetime
+from django.utils.timezone import now
 from kk.models import Hearing, Label
 from kk.tests.base import BaseKKDBTest
 
 class TestHearing(BaseKKDBTest):
     def setup(self):
         super(TestHearing, self).setup()
-
-        self.hearing_one = Hearing(abstract='Hearing One')
-        self.hearing_two = Hearing(abstract='Hearing Two')
-        self.hearing_three = Hearing(abstract='Hearing Three')
-
-        # save hearings
-        self.hearing_one.save()
-        self.hearing_two.save()
-        self.hearing_three.save()
-       
         self.endpoint = '%s/hearing/' % self.base_endpoint
         self.list_endpoint = '%s?format=json' % self.endpoint
-   
+
     def get_detail_url(self, id):
         return '%s%s/?format=json' % (self.endpoint, id)
 
     def create_hearings(self, n):
-        for i in range(1, n+1):
-            Hearing(abstract='Test purpose created hearing %s' % str(i)).save()
+        Hearing.objects.all().delete()  # Get rid of all other hearings
+        hearings = []
+
+        # Depending on the database backend, created_at dates (which are used for ordering)
+        # may be truncated to the closest second, so we purposefully backdate these
+        # to ensure ordering on all platforms.
+        for i in range(n):
+            hearings.append(
+                Hearing.objects.create(
+                    abstract='Test purpose created hearing %s' % (i + 1),
+                    created_at=now() - datetime.timedelta(seconds = 1 + (n - i))
+                )
+            )
+        return hearings
 
     def test_list_all_hearings_no_objects(self):
-        self.hearing_one.delete()
-        self.hearing_two.delete()
-        self.hearing_three.delete()
-
+        self.create_hearings(0)
         response = self.client.get(self.list_endpoint)
         assert response.status_code is 200
-
         data = self.get_data_from_response(response)
         assert len(data) == 0
 
@@ -44,20 +44,17 @@ class TestHearing(BaseKKDBTest):
         assert response.status_code is 200
 
         data = self.get_data_from_response(response)
-        assert len(data) == 3
-       
+        assert len(data) == Hearing.objects.count()
+
     def test_list_all_hearings_check_abstract(self):
+        hearings = self.create_hearings(3)
         response = self.client.get(self.list_endpoint)
         assert response.status_code is 200
 
         data = self.get_data_from_response(response)
-        abstracts = []
-        for o in data:
-            abstracts.append(o['abstract'])
-
-        assert self.hearing_three.abstract in abstracts
-        assert self.hearing_two.abstract in abstracts
-        assert self.hearing_one.abstract in abstracts
+        assert data[0]['abstract'] == hearings[2].abstract
+        assert data[1]['abstract'] == hearings[1].abstract
+        assert data[2]['abstract'] == hearings[0].abstract
 
     def test_list_top_5_hearings_check_number_of_objects(self):
         self.create_hearings(10)
@@ -81,65 +78,20 @@ class TestHearing(BaseKKDBTest):
         assert '7' in objects[3]['abstract']
         assert '6' in objects[4]['abstract']
 
-    def test_get_next_closing_hearing_one_available(self):
-        first_close_at = datetime.datetime.now()+datetime.timedelta(days=1)
-        third_close_at = datetime.datetime.now()+datetime.timedelta(days=3)
-        hearing_one = Hearing(abstract='Closing hearing one')
-        hearing_two = Hearing(abstract='Closing hearing two')
-        self.hearing_one.close_at = third_close_at
-
-        # hearing_one will close today
-        # hearing_two will close today
-        # First hearing created during setup will close in three days
-
-        # We expect first hearing created in a setup which is next for our first_close_at date
-
-        hearing_one.save()
-        hearing_two.save()
-        self.hearing_one.save()
-
-        url = '%s&next_closing=%s' % (self.list_endpoint, urllib.parse.quote_plus(first_close_at.strftime('%Y-%m-%dT%H:%M:%S.%uZ')))
-        #url = '%s&next_closing=%s&limit=1' % (self.list_endpoint, urllib.parse.quote_plus(first_close_at.strftime('%Y-%m-%dT%H:%M:%S.%uZ')))      
-        response = self.client.get(url)
-        assert response.status_code is 200
-
+    def test_get_next_closing_hearings(self):
+        self.create_hearings(0)  # Clear out old hearings
+        closed_hearing_1 = Hearing.objects.create(abstract='Gone', close_at=now() - datetime.timedelta(days=1))
+        closed_hearing_2 = Hearing.objects.create(abstract='Gone too', close_at=now() - datetime.timedelta(days=2))
+        future_hearing_1 = Hearing.objects.create(abstract='Next up', close_at=now() + datetime.timedelta(days=1))
+        future_hearing_2 = Hearing.objects.create(abstract='Next up', close_at=now() + datetime.timedelta(days=5))
+        response = self.client.get('%s&next_closing=%s' % (self.list_endpoint, urllib.parse.quote_plus(now().isoformat())))
         data = self.get_data_from_response(response)
-
-        assert len(data) is 1
-        assert data[0]['abstract'] == self.hearing_one.abstract
-
-    def test_get_next_closing_hearing_two_available(self):
-        first_close_at = datetime.datetime.now()+datetime.timedelta(days=1)
-        second_close_at = datetime.datetime.now()+datetime.timedelta(days=2)
-        third_close_at = datetime.datetime.now()+datetime.timedelta(days=3)
-        hearing_one = Hearing(abstract='Closing hearing one', close_at=first_close_at)
-        hearing_two = Hearing(abstract='Closing hearing two', close_at=second_close_at)
-        self.hearing_one.close_at = third_close_at
-
-        # hearing_one will close in one day
-        # hearing_two will close in two days
-        # First hearing created during setup will close in three days
-
-        # We expect hearing_two which is next for our hearing_one
-
-        hearing_one.save()
-        hearing_two.save()
-        self.hearing_one.save()
-
-        # add one second to datetime
-        # at least it fixes the same datetime returned issue with SQLite in my case
-        # e.g. &next_closing=2015-11-03T08%3A28%3A09.2Z returns object which close_at is 2015-11-03T08:28:09
-        # though, query constraint is 'close_at > 2015-11-03T08:28:09'
-        next_close_date = first_close_at + datetime.timedelta(0,1)
-        url = '%s&next_closing=%s' % (self.list_endpoint, urllib.parse.quote_plus(next_close_date.strftime('%Y-%m-%dT%H:%M:%S.%uZ')))
-        #url = '%s&next_closing=%s&limit=1' % (self.list_endpoint, urllib.parse.quote_plus(next_close_date.strftime('%Y-%m-%dT%H:%M:%S.%uZ')))
-        response = self.client.get(url)
-        assert response.status_code is 200
-
+        assert len(data) == 1
+        assert data[0]['abstract'] == future_hearing_1.abstract
+        response = self.client.get('%s&next_closing=%s' % (self.list_endpoint, urllib.parse.quote_plus(future_hearing_1.close_at.isoformat())))
         data = self.get_data_from_response(response)
-
-        assert len(data) is 1
-        assert data[0]['abstract'] == hearing_two.abstract
+        assert len(data) == 1
+        assert data[0]['abstract'] == future_hearing_2.abstract
 
     def test_8_get_detail_abstract(self):
         hearing = Hearing(abstract='Lorem Ipsum Abstract')
@@ -149,7 +101,7 @@ class TestHearing(BaseKKDBTest):
         assert response.status_code is 200
 
         data = self.get_data_from_response(response)
-        
+
         assert 'results' not in data
         assert data['abstract'] == hearing.abstract
 
@@ -161,7 +113,7 @@ class TestHearing(BaseKKDBTest):
         assert response.status_code is 200
 
         data = self.get_data_from_response(response)
-        
+
         assert 'results' not in data
         assert data['heading'] == hearing.heading
 
@@ -173,7 +125,7 @@ class TestHearing(BaseKKDBTest):
         assert response.status_code is 200
 
         data = self.get_data_from_response(response)
-        
+
         assert 'results' not in data
         assert data['borough'] == hearing.borough
 
@@ -185,7 +137,7 @@ class TestHearing(BaseKKDBTest):
         assert response.status_code is 200
 
         data = self.get_data_from_response(response)
-        
+
         assert 'results' not in data
         assert data['n_comments'] == hearing.n_comments
 
@@ -197,9 +149,10 @@ class TestHearing(BaseKKDBTest):
         assert response.status_code is 200
 
         data = self.get_data_from_response(response)
-        
+
         assert 'results' not in data
-        assert data['close_at'] == hearing.close_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        # Allow for subsecond inaccuracy
+        assert (parse_datetime(data['close_at']) - hearing.close_at).total_seconds() < 1
 
     def test_8_get_detail_labels(self):
         hearing = Hearing()
@@ -218,17 +171,17 @@ class TestHearing(BaseKKDBTest):
         assert response.status_code is 200
 
         data = self.get_data_from_response(response)
-        
+
         assert 'results' not in data
         assert len(data['labels']) is 3
         assert label_one.label in data['labels']
 
     def test_8_get_detail_empty(self):
-        raise NotImplementedError("Add tests for empty values")
+        pytest.xfail("Add tests for empty values")
 
     def test_8_get_detail_invalid(self):
-        raise NotImplementedError("Add tests for invalid values")
-    
+        pytest.xfail("Add tests for invalid values")
+
     def test_7_get_detail_location(self):
         hearing = Hearing(latitude='60.19276', longitude='24.93300')
         hearing.save()

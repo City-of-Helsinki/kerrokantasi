9ac4bac5dbe30d36f14de1cb47acfe07eaf713ad
piotras <piotrek.pokora@gmail.com>
2015-11-03 13:08:12 +0100

    List all images added to hearing. Closes #37.

--- kk/tests/test_images.py
+++ kk/tests/test_images.py
@@ -23,7 +23,7 @@ class TestImage(BaseKKDBTest):
     def create_hearing_image(self, hearing, name):
         # TODO: copy images to IMAGES_DIR if required
         path = '%s/%s' % (settings.IMAGES_DIR, name)
-        image = HearingImage(hearing=hearing, image=path)
+        image = HearingImage(hearing=hearing, image=path, title=name)
         image.save()
 
     def test_8_list_hearing_images_check_number_of_images(self):
@@ -54,3 +54,28 @@ class TestImage(BaseKKDBTest):
         assert 'original' in urls[0]
         assert 'small' in urls[1]
         assert 'thumbnail' in urls[2]
+
+    def test_37_list_hearing_images_check_number_of_images(self):
+        self.create_hearing_image(self.hearing, 'original.jpg')
+        self.create_hearing_image(self.hearing, 'small.jpg')
+        self.create_hearing_image(self.hearing, 'thumbnail.jpg')
+
+        # /v1/hearing/<hearingID>/images/
+        url = '%s%s/images/?format=json' % (self.hearing_endpoint, self.hearing.id)
+        response = self.client.get(url)
+        data = self.get_data_from_response(response)
+
+        assert len(data) is 3
+
+    def test_37_list_hearing_images_check_titles(self):
+        self.create_hearing_image(self.hearing, 'original.jpg')
+        self.create_hearing_image(self.hearing, 'small.jpg')
+        self.create_hearing_image(self.hearing, 'thumbnail.jpg')
+
+        url = '%s%s/images/?format=json' % (self.hearing_endpoint, self.hearing.id)
+        response = self.client.get(url)
+        data = self.get_data_from_response(response)
+
+        assert data[0]['title'] == 'original.jpg'
+        assert data[1]['title'] == 'small.jpg'
+        assert data[2]['title'] == 'thumbnail.jpg'

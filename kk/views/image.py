e67c3fb492c142a6ef5bbb72ec3abe031595fa40
piotras <piotrek.pokora@gmail.com>
2015-11-03 13:45:51 +0100

    Image serializers moved to image file

---
+++ kk/views/image.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from kk.models import HearingImage
+
+# Serializer for image
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HearingImage
+        fields = ['title', 'image', 'width', 'height']
+
+# Serializer for 'image' field.
+class ImageFieldSerializer(serializers.RelatedField):
+    def to_representation(self, image):
+        return {'name': image.title, 'url': image.image.url, 'type': image.type,
+                'width': image.width, 'height': image.height}

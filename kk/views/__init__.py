2b0c3b1b066f80f80b8a5e2b36a2d1cb22566d2b
piotras <piotrek.pokora@gmail.com>
2015-11-09 10:07:22 +0100

    Import ScenarioCommentViewSet

--- kk/views/__init__.py
+++ kk/views/__init__.py
@@ -1,3 +1,2 @@
 from .hearing import HearingViewSet
-from .scenario import ScenarioViewSet
-from .comment import ScenarioCommentViewSet
+from .scenario import ScenarioViewSet, ScenarioCommentViewSet

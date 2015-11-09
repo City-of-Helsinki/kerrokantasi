6ae5016bdf2b4cb754a473519ffd3297f4df60cf
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-11-09 12:20:04 +0200

    kk_mock_populate: Create users and comments

--- kk/management/commands/kk_mock_populate.py
+++ kk/management/commands/kk_mock_populate.py
@@ -1,18 +1,20 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from kk.factories.hearing import HearingFactory, LabelFactory, ScenarioFactory
-from kk.models import Hearing, Label, Scenario
+from kk.factories.hearing import HearingFactory, LabelFactory
+from kk.factories.user import UserFactory
+from kk.models import Hearing, Label
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
         if settings.AUTH_USER_MODEL == "auth.User":
             from django.contrib.auth.models import User
             if not User.objects.filter(username="admin").exists():
                 User.objects.create_superuser(username="admin", email="admin@example.com", password="admin")
                 print("Admin user 'admin' (password 'admin') created")
-
+            while User.objects.count() < 25:
+                user = UserFactory()
+                print("Created user %s" % user.pk)
         while Label.objects.count() < 5:
             label = LabelFactory()
             print("Created label %s" % label.pk)

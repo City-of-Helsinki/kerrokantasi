580af07f5b2f14f67331f1d9038bb4951107e9c9
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-11-23 15:13:11 +0200

    Pytestify comment tests; improve coverage

--- kk/tests/conftest.py
+++ kk/tests/conftest.py
@@ -2,13 +2,17 @@ import pytest
 from django.contrib.auth import get_user_model
 from kk.enums import SectionType, Commenting
 from kk.factories.hearing import HearingFactory, LabelFactory
-from kk.models import Hearing, Section, Label
+from kk.models import Hearing, Section, Label, HearingComment
 from kk.tests.utils import create_default_images, assert_ascending_sequence
 from rest_framework.test import APIClient
 
+default_comment_content = 'I agree with you sir Lancelot. My favourite colour is blue'
+red_comment_content = 'Mine is red'
+green_comment_content = 'I like green'
+
 
 @pytest.fixture()
-def default_hearing():
+def default_hearing(john_doe):
     """
     Fixture for a "default" hearing with three sections (one introduction, two sections).
     All objects will have the 3 default images attached.
@@ -25,6 +29,11 @@ def default_hearing():
         )
         create_default_images(section)
     assert_ascending_sequence([s.ordering for s in hearing.sections.all()])
+
+    hearing.comments.add(HearingComment(created_by=john_doe, content=default_comment_content))
+    hearing.comments.add(HearingComment(created_by=john_doe, content=red_comment_content))
+    hearing.comments.add(HearingComment(created_by=john_doe, content=green_comment_content))
+
     return hearing
 
 
@@ -52,6 +61,7 @@ def john_doe():
 def john_doe_api_client(john_doe):
     api_client = APIClient()
     api_client.login(username=john_doe.username, password="password")
+    api_client.user = john_doe
     return api_client
 
 
@@ -59,4 +69,10 @@ def john_doe_api_client(john_doe):
 def admin_api_client(admin_user):
     api_client = APIClient()
     api_client.login(username=admin_user.username, password="password")
+    api_client.user = admin_user
     return api_client
+
+
+@pytest.fixture()
+def api_client():
+    return APIClient()
--- kk/tests/test_comment_vote.py
+++ kk/tests/test_comment_vote.py
@@ -1,140 +1,150 @@
 import pytest
-import datetime
-import urllib
-import os
 
-from django.conf import settings
+from kk.models import HearingComment, Section, SectionComment
+from kk.tests.test_images import get_hearing_detail_url
 
-from kk.models import Hearing, HearingComment, Section, SectionComment
-from kk.tests.base import BaseKKDBTest, default_hearing
+default_content = 'Awesome comment to vote.'
+comment_data = {
+    'content': default_content,
+    'section': None
+}
 
 
-class TestCommentVote(BaseKKDBTest):
-    def setup(self):
-        super(TestCommentVote, self).setup()
+def add_default_section_and_comment(hearing):
+    section = Section.objects.create(title='Section title', hearing=hearing)
+    comment = SectionComment.objects.create(content='Comment text', section=section)
+    return [section, comment]
 
-        self.default_content = 'Awesome comment to vote.'
-        self.comment_data = {
-            'content': self.default_content,
-            'section': None
-        }
 
-    def add_default_section_and_comment(self, hearing):
-        section = Section.objects.create(title='Section title', hearing=hearing)
-        comment = SectionComment.objects.create(content='Comment text', section=section)
-        return [section, comment]
+def get_section_comment_vote_url(hearing_id, section_id, comment_id):
+    # /v1/hearings/<hearingID>/sections/<sectionID>/comments/<commentID>/votes/
+    return get_hearing_detail_url(hearing_id, 'sections/%s/comments/%s/vote' % (section_id, comment_id))
 
-    def get_section_comment_vote_url(self, hearing_id, section_id, comment_id):
-        # /v1/hearings/<hearingID>/sections/<sectionID>/comments/<commentID>/votes/
-        return self.get_hearing_detail_url(hearing_id, 'sections/%s/comments/%s/vote' % (section_id, comment_id))
 
-    def add_default_hearing_comment(self, hearing):
-        return HearingComment.objects.create(content='Comment text', hearing=hearing)
+def add_default_hearing_comment(hearing):
+    return HearingComment.objects.create(content='Comment text', hearing=hearing)
 
-    def get_hearing_comment_vote_url(self, hearing_id, comment_id):
-        # /v1/hearings/<hearingID>/comments/<commentID>/votes/
-        return self.get_hearing_detail_url(hearing_id, 'comments/%s/vote' % comment_id)
 
-    def get_hearing_comment_unvote_url(self, hearing_id, comment_id):
-        # /v1/hearings/<hearingID>/comments/<commentID>/unvotes/
-        return self.get_hearing_detail_url(hearing_id, 'comments/%s/unvote' % comment_id)
+def get_hearing_comment_vote_url(hearing_id, comment_id):
+    # /v1/hearings/<hearingID>/comments/<commentID>/votes/
+    return get_hearing_detail_url(hearing_id, 'comments/%s/vote' % comment_id)
 
-    def test_31_section_comment_vote_without_authentication(self, default_hearing):
-        section, comment = self.add_default_section_and_comment(default_hearing)
-        response = self.client.post(self.get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
-        assert response.status_code == 403
 
-    def test_31_hearing_comment_vote_without_authentication(self, default_hearing):
-        comment = self.add_default_hearing_comment(default_hearing)
-        response = self.client.post(self.get_hearing_comment_vote_url(default_hearing.id, comment.id))
-        assert response.status_code == 403
+def get_hearing_comment_unvote_url(hearing_id, comment_id):
+    # /v1/hearings/<hearingID>/comments/<commentID>/unvotes/
+    return get_hearing_detail_url(hearing_id, 'comments/%s/unvote' % comment_id)
 
-    def test_31_section_comment_vote_add_vote(self, default_hearing):
-        self.user_login()
-        section, comment = self.add_default_section_and_comment(default_hearing)
-        response = self.client.post(self.get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
-        assert response.status_code == 201
 
-    def test_31_hearing_comment_vote_add_vote(self, default_hearing):
-        self.user_login()
-        comment = self.add_default_hearing_comment(default_hearing)
-        response = self.client.post(self.get_hearing_comment_vote_url(default_hearing.id, comment.id))
-        assert response.status_code == 201
+@pytest.mark.django_db
+def test_31_section_comment_vote_without_authentication(client, default_hearing):
+    section, comment = add_default_section_and_comment(default_hearing)
+    response = client.post(get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
+    assert response.status_code == 403
 
-    def test_31_section_comment_vote_add_vote_check_voter(self, default_hearing):
-        self.user_login()
-        section, comment = self.add_default_section_and_comment(default_hearing)
-        response = self.client.post(self.get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
-        assert response.status_code == 201
 
-        comments = SectionComment.objects.filter(id=comment.id, voters=self.user)
-        assert len(comments) == 1
+@pytest.mark.django_db
+def test_31_hearing_comment_vote_without_authentication(client, default_hearing):
+    comment = add_default_hearing_comment(default_hearing)
+    response = client.post(get_hearing_comment_vote_url(default_hearing.id, comment.id))
+    assert response.status_code == 403
 
-    def test_31_hearing_comment_vote_add_vote_check_voter(self, default_hearing):
-        self.user_login()
-        comment = self.add_default_hearing_comment(default_hearing)
-        response = self.client.post(self.get_hearing_comment_vote_url(default_hearing.id, comment.id))
-        assert response.status_code == 201
 
-        comments = HearingComment.objects.filter(id=comment.id, voters=self.user)
-        assert len(comments) == 1
+@pytest.mark.django_db
+def test_31_section_comment_vote_add_vote(john_doe_api_client, default_hearing):
+    section, comment = add_default_section_and_comment(default_hearing)
+    response = john_doe_api_client.post(get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
+    assert response.status_code == 201
 
-    def test_31_section_comment_vote_add_vote_check_amount_of_votes(self, default_hearing):
-        self.user_login()
-        section, comment = self.add_default_section_and_comment(default_hearing)
-        response = self.client.post(self.get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
-        assert response.status_code == 201
 
-        comment = SectionComment.objects.get(id=comment.id, voters=self.user)
-        assert comment.voters.all().count() == 1
-        assert comment.n_votes == 1
+@pytest.mark.django_db
+def test_31_hearing_comment_vote_add_vote(john_doe_api_client, default_hearing):
+    comment = add_default_hearing_comment(default_hearing)
+    response = john_doe_api_client.post(get_hearing_comment_vote_url(default_hearing.id, comment.id))
+    assert response.status_code == 201
 
-    def test_31_hearing_comment_vote_add_vote_check_amount_of_votes(self, default_hearing):
-        self.user_login()
-        comment = self.add_default_hearing_comment(default_hearing)
-        response = self.client.post(self.get_hearing_comment_vote_url(default_hearing.id, comment.id))
-        assert response.status_code == 201
 
-        comment = HearingComment.objects.get(id=comment.id, voters=self.user)
-        assert comment.voters.all().count() == 1
-        assert comment.n_votes == 1
+@pytest.mark.django_db
+def test_31_section_comment_vote_add_vote_check_voter(john_doe_api_client, default_hearing):
+    section, comment = add_default_section_and_comment(default_hearing)
+    response = john_doe_api_client.post(get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
+    assert response.status_code == 201
 
-    def test_31_section_comment_vote_add_second_vote(self, default_hearing):
-        self.user_login()
-        section, comment = self.add_default_section_and_comment(default_hearing)
-        response = self.client.post(self.get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
-        assert response.status_code == 201
-        # Add vote again. Expect error, because vote has been given already.
-        response = self.client.post(self.get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
-        assert response.status_code == 304
+    comments = SectionComment.objects.filter(id=comment.id, voters=john_doe_api_client.user)
+    assert len(comments) == 1
 
-    def test_31_hearing_comment_vote_add_second_vote(self, default_hearing):
-        self.user_login()
-        comment = self.add_default_hearing_comment(default_hearing)
-        response = self.client.post(self.get_hearing_comment_vote_url(default_hearing.id, comment.id))
-        assert response.status_code == 201
-        # Add vote again. Expect error, because vote has been given already.
-        response = self.client.post(self.get_hearing_comment_vote_url(default_hearing.id, comment.id))
-        assert response.status_code == 304
 
-    def test_hearing_comment_unvote(self, default_hearing):
-        comment = self.add_default_hearing_comment(default_hearing)
-        response = self.client.post(self.get_hearing_comment_unvote_url(default_hearing.id, comment.id))
-        assert response.status_code == 403
-        self.user_login()
-        self.client.post(self.get_hearing_comment_vote_url(default_hearing.id, comment.id))
-        response = self.client.post(self.get_hearing_comment_unvote_url(default_hearing.id, comment.id))
-        assert response.status_code == 204
-        assert comment.n_votes == 0
-        response = self.client.post(self.get_hearing_comment_unvote_url(default_hearing.id, comment.id))
-        assert response.status_code == 304
+@pytest.mark.django_db
+def test_31_hearing_comment_vote_add_vote_check_voter(john_doe_api_client, default_hearing):
+    comment = add_default_hearing_comment(default_hearing)
+    response = john_doe_api_client.post(get_hearing_comment_vote_url(default_hearing.id, comment.id))
+    assert response.status_code == 201
 
-    def test_vote_appears_in_user_data(self, default_hearing):
-        self.user_login()
-        comment = self.add_default_hearing_comment(default_hearing)
-        self.client.post(self.get_hearing_comment_vote_url(default_hearing.id, comment.id))
-        scenario, sc_comment = self.add_default_scenario_and_comment(default_hearing)
-        self.client.post(self.get_scenario_comment_vote_url(default_hearing.id, scenario.id, sc_comment.id))
-        response = self.client.get('/v1/users/')
-        assert comment.id in response.data[0]['voted_hearing_comments']
-        assert sc_comment.id in response.data[0]['voted_scenario_comments']
+    comments = HearingComment.objects.filter(id=comment.id, voters=john_doe_api_client.user)
+    assert len(comments) == 1
+
+
+@pytest.mark.django_db
+def test_31_section_comment_vote_add_vote_check_amount_of_votes(john_doe_api_client, default_hearing):
+    section, comment = add_default_section_and_comment(default_hearing)
+    response = john_doe_api_client.post(get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
+    assert response.status_code == 201
+
+    comment = SectionComment.objects.get(id=comment.id, voters=john_doe_api_client.user)
+    assert comment.voters.all().count() == 1
+    assert comment.n_votes == 1
+
+
+@pytest.mark.django_db
+def test_31_hearing_comment_vote_add_vote_check_amount_of_votes(john_doe_api_client, default_hearing):
+    comment = add_default_hearing_comment(default_hearing)
+    response = john_doe_api_client.post(get_hearing_comment_vote_url(default_hearing.id, comment.id))
+    assert response.status_code == 201
+
+    comment = HearingComment.objects.get(id=comment.id, voters=john_doe_api_client.user)
+    assert comment.voters.all().count() == 1
+    assert comment.n_votes == 1
+
+
+@pytest.mark.django_db
+def test_31_section_comment_vote_add_second_vote(john_doe_api_client, default_hearing):
+    section, comment = add_default_section_and_comment(default_hearing)
+    response = john_doe_api_client.post(get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
+    assert response.status_code == 201
+    # Add vote again. Expect error, because vote has been given already.
+    response = john_doe_api_client.post(get_section_comment_vote_url(default_hearing.id, section.id, comment.id))
+    assert response.status_code == 304
+
+
+@pytest.mark.django_db
+def test_31_hearing_comment_vote_add_second_vote(john_doe_api_client, default_hearing):
+    comment = add_default_hearing_comment(default_hearing)
+    response = john_doe_api_client.post(get_hearing_comment_vote_url(default_hearing.id, comment.id))
+    assert response.status_code == 201
+    # Add vote again. Expect error, because vote has been given already.
+    response = john_doe_api_client.post(get_hearing_comment_vote_url(default_hearing.id, comment.id))
+    assert response.status_code == 304
+
+
+@pytest.mark.django_db
+def test_hearing_comment_unvote(client, john_doe_api_client, default_hearing):
+    comment = add_default_hearing_comment(default_hearing)
+    response = client.post(get_hearing_comment_unvote_url(default_hearing.id, comment.id))
+    assert response.status_code == 403
+
+    john_doe_api_client.post(get_hearing_comment_vote_url(default_hearing.id, comment.id))
+    response = john_doe_api_client.post(get_hearing_comment_unvote_url(default_hearing.id, comment.id))
+    assert response.status_code == 204
+    assert comment.n_votes == 0
+    response = john_doe_api_client.post(get_hearing_comment_unvote_url(default_hearing.id, comment.id))
+    assert response.status_code == 304
+
+
+@pytest.mark.django_db
+def test_vote_appears_in_user_data(john_doe_api_client, default_hearing):
+    comment = add_default_hearing_comment(default_hearing)
+    john_doe_api_client.post(get_hearing_comment_vote_url(default_hearing.id, comment.id))
+    section, sc_comment = add_default_section_and_comment(default_hearing)
+    john_doe_api_client.post(get_section_comment_vote_url(default_hearing.id, section.id, sc_comment.id))
+    response = john_doe_api_client.get('/v1/users/')
+    assert comment.id in response.data[0]['voted_hearing_comments']
+    assert sc_comment.id in response.data[0]['voted_section_comments']

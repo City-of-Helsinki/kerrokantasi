a0cb3e12e04c67310ce89360e722a9ff92ea5b8e
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-12-07 14:51:48 +0200

    Use django-nested-admin for Hearings
    Refs #118

--- democracy/admin/__init__.py
+++ democracy/admin/__init__.py
@@ -1,41 +1,36 @@
 from django.contrib import admin
+from nested_admin.nested import NestedAdmin, NestedStackedInline
 
 from democracy import models
 
 
 # Inlines
 
-class SectionInline(admin.StackedInline):
-    model = models.Section
-    extra = 0
-    show_change_link = True
 
-
-class HearingImageInline(admin.StackedInline):
+class HearingImageInline(NestedStackedInline):
     model = models.HearingImage
     extra = 0
 
 
-class SectionImageInline(admin.StackedInline):
+class SectionImageInline(NestedStackedInline):
     model = models.SectionImage
     extra = 0
+
+
+class SectionInline(NestedStackedInline):
+    model = models.Section
+    extra = 1
+    inlines = [SectionImageInline]
 
 
 # Admins
 
 
-class HearingAdmin(admin.ModelAdmin):
+class HearingAdmin(NestedAdmin):
     inlines = [HearingImageInline, SectionInline]
     list_display = ("id", "published", "title", "open_at", "close_at", "force_closed")
     list_filter = ("published",)
     search_fields = ("id", "title")
-
-
-class SectionAdmin(admin.ModelAdmin):
-    list_filter = ("hearing", "published", "type")
-    list_display = ("id", "published", "type", "hearing", "title")
-    list_display_links = list_display
-    inlines = [SectionImageInline]
 
 
 # Wire it up!
@@ -43,4 +38,3 @@ class SectionAdmin(admin.ModelAdmin):
 
 admin.site.register(models.Label)
 admin.site.register(models.Hearing, HearingAdmin)
-admin.site.register(models.Section, SectionAdmin)
--- kerrokantasi/urls.py
+++ kerrokantasi/urls.py
@@ -1,3 +1,4 @@
+from nested_admin import urls as nested_admin_urls
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
@@ -6,6 +7,7 @@ from democracy import urls_v1
 
 urlpatterns = [
     url(r'^v1/', include(urls_v1)),
+    url(r'^nested_admin/', include(nested_admin_urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
 

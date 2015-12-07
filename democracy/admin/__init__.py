9e02f92fc19b7f833b25d0273143e98261a3b484
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-12-07 14:55:49 +0200

    Hide unnecessary fields in the admins
    * Hide some unnecessary fields from Hearings
    * Hide Public and Commenting flags from Sections
      (Section commenting option follows that of hearings.)
    * Hide Public and Title fields from images
    * Hide Public field from labels
    
    Refs #118

--- democracy/admin/__init__.py
+++ democracy/admin/__init__.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from nested_admin.nested import NestedAdmin, NestedStackedInline
 
 from democracy import models
@@ -10,17 +11,20 @@ from democracy import models
 class HearingImageInline(NestedStackedInline):
     model = models.HearingImage
     extra = 0
+    exclude = ("public", "title")
 
 
 class SectionImageInline(NestedStackedInline):
     model = models.SectionImage
     extra = 0
+    exclude = ("public", "title")
 
 
 class SectionInline(NestedStackedInline):
     model = models.Section
     extra = 1
     inlines = [SectionImageInline]
+    exclude = ("public", "commenting",)
 
 
 # Admins
@@ -31,10 +35,28 @@ class HearingAdmin(NestedAdmin):
     list_display = ("id", "published", "title", "open_at", "close_at", "force_closed")
     list_filter = ("published",)
     search_fields = ("id", "title")
+    fieldsets = (
+        (None, {
+            "fields": ("title", "abstract", "labels", "id")
+        }),
+        (_("Availability"), {
+            "fields": ("published", "open_at", "close_at", "force_closed", "commenting")
+        }),
+    )
+
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        hearing = form.instance
+        assert isinstance(hearing, models.Hearing)
+        hearing.sections.update(commenting=hearing.commenting)
+
+
+class LabelAdmin(admin.ModelAdmin):
+    exclude = ("public",)
 
 
 # Wire it up!
 
 
-admin.site.register(models.Label)
+admin.site.register(models.Label, LabelAdmin)
 admin.site.register(models.Hearing, HearingAdmin)

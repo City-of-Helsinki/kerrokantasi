c9402a67e516f4ea764bee5a50e87a8a2c4bc31e
Tuomas Haapala <tuomas.haapala@anders.fi>
2016-02-24 14:11:04 +0200

    Reset comment cache when copying a hearing as a draft

--- democracy/models/utils.py
+++ democracy/models/utils.py
@@ -31,6 +31,7 @@ def copy_hearing(old_hearing, **kwargs):
     for key, value in kwargs.items():
         setattr(new_hearing, key, value)
 
+    new_hearing.n_comments = 0
     new_hearing.save()
     new_hearing.labels = old_hearing.labels.all()
 
@@ -40,6 +41,7 @@ def copy_hearing(old_hearing, **kwargs):
         old_images = section.images.all()
         section.pk = None
         section.hearing = new_hearing
+        section.n_comments = 0
         section.save()
         for image in old_images:
             image.pk = None

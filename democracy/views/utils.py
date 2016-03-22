01331f37c629f0738c5517527a6e78be55334e04
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-03-22 09:52:19 +0200

    Allow overriding the ManyRelatedField for image fields

--- democracy/views/utils.py
+++ democracy/views/utils.py
@@ -2,24 +2,36 @@
 from functools import lru_cache
 
 from rest_framework import serializers
+from rest_framework.relations import ManyRelatedField, MANY_RELATION_KWARGS
 
 
 class AbstractFieldSerializer(serializers.RelatedField):
     parent_serializer_class = serializers.ModelSerializer
+    many_field_class = ManyRelatedField
 
     def to_representation(self, image):
         return self.parent_serializer_class(image, context=self.context).data
+
+    @classmethod
+    def many_init(cls, *args, **kwargs):
+        list_kwargs = {'child_relation': cls(*args, **kwargs)}
+        for key in kwargs.keys():
+            if key in MANY_RELATION_KWARGS:
+                list_kwargs[key] = kwargs[key]
+        return cls.many_field_class(**list_kwargs)
 
 
 class AbstractSerializerMixin(object):
 
     @classmethod
     @lru_cache()
-    def get_field_serializer_class(cls):
+    def get_field_serializer_class(cls, many_field_class=ManyRelatedField):
         return type('%sFieldSerializer' % cls.Meta.model, (AbstractFieldSerializer,), {
-            "parent_serializer_class": cls
+            "parent_serializer_class": cls,
+            "many_field_class": many_field_class,
         })
 
     @classmethod
     def get_field_serializer(cls, **kwargs):
-        return cls.get_field_serializer_class()(**kwargs)
+        many_field_class = kwargs.pop("many_field_class", ManyRelatedField)
+        return cls.get_field_serializer_class(many_field_class=many_field_class)(**kwargs)

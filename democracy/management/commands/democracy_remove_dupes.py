918687438f679ded6c3e1c291a7c31d4ac373d9c
Rikuoja <riku.oja@kapsi.fi>
2016-07-20 16:54:02 +0300

    Also remove empty comments with duplicate plugin_data

--- democracy/management/commands/democracy_remove_dupes.py
+++ democracy/management/commands/democracy_remove_dupes.py
@@ -13,25 +13,34 @@ class Command(BaseCommand):
     )
 
     def _remove_dupes(self, klass):
-        potential_dupes = klass.objects.values('content',).exclude(content='')\
+        # detect the dupes primarily by content
+        potential_dupes = klass.objects.values('content',)\
             .exclude(deleted=True).annotate(Count('content'))\
             .filter(content__count__gt=1).order_by('-content__count')
 
+        # further filter by creation time, parent hearing and plugin data
         for d in potential_dupes:
             objs = list(klass.objects.filter(content=d['content']).order_by('created_at'))
             first = objs.pop(0)
-            print("%s\n%s" % (first.created_at, first.content))
+            print("%s %s\n%s\n%s" % (getattr(first, klass.parent_field),
+                                     first.created_at,
+                                     first.content,
+                                     first.plugin_data))
             for other in objs:
+                if other.plugin_data != first.plugin_data:
+                    print("\tplugin data differs %s" % other)
+                    continue
                 if other.created_at - first.created_at > timedelta(hours=1):
                     print("\ttoo late %s" % (other.created_at - first.created_at))
                     continue
                 if getattr(other, klass.parent_field) != getattr(first, klass.parent_field):
                     print("\tdifferent parent %s" % other)
                     continue
-                if other.plugin_data != first.plugin_data:
-                    print("\tplugin data differs %s" % other)
-                    continue
                 print("\tbye bye %s" % other)
+                # transfer any votes to the existing comment
+                first.n_legacy_votes += other.n_legacy_votes
+                first.voters = first.voters.all() | other.voters.all()
+                first.save()
                 other.soft_delete()
 
     def handle(self, *args, **options):

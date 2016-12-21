0f5bf394920787cc7333a0ebb538f219eb70671c
Rikuoja <riku.oja@kapsi.fi>
2016-12-21 17:55:34 +0200

    Add label name filter for autocomplete purposes

--- democracy/views/label.py
+++ democracy/views/label.py
@@ -1,7 +1,15 @@
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, filters
+import django_filters
 
 from democracy.models import Label
 from democracy.pagination import DefaultLimitPagination
+
+class LabelFilter(django_filters.FilterSet):
+    label = django_filters.CharFilter(lookup_type='icontains')
+
+    class Meta:
+        model = Label
+        fields = ['label']
 
 
 class LabelSerializer(serializers.ModelSerializer):
@@ -14,3 +22,5 @@ class LabelViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = LabelSerializer
     queryset = Label.objects.all()
     pagination_class = DefaultLimitPagination
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = LabelFilter

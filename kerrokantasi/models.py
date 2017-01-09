9160ecaa68a417b0e479a82ce08000505d66efc6
Stephane Weiss <stephane.weiss@anders.fi>
2017-01-09 15:39:53 +0200

    Add a field nickname to user
    Add a migration to add the new field
    Update the get_display_name function to display first the nickname if
    any, else the real name

--- kerrokantasi/models.py
+++ kerrokantasi/models.py
@@ -1,12 +1,19 @@
+from django.db import models
 from helusers.models import AbstractUser
 
 
 class User(AbstractUser):
+
+    nickname = models.CharField(max_length=50, blank=True)
+
     def __str__(self):
         return ' - '.join([super().__str__(), self.get_display_name(), self.email])
 
-    def get_display_name(self):
+    def get_real_name(self):
         return '{0} {1}'.format(self.first_name, self.last_name).strip()
+
+    def get_display_name(self):
+        return self.nickname or self.get_real_name()
 
     def get_default_organization(self):
         return self.admin_organizations.order_by('created_at').first()

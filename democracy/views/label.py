7ae615705ea62aff98bec2fe85de5a407c65f066
Stephane Weiss <stephane.weiss@anders.fi>
2017-01-13 15:04:13 +0200

    Update the HearingFilter and LabelFilter for translations

--- democracy/views/label.py
+++ democracy/views/label.py
@@ -7,7 +7,7 @@ from democracy.views.utils import TranslatableSerializer
 
 
 class LabelFilter(django_filters.FilterSet):
-    label = django_filters.CharFilter(lookup_type='icontains')
+    label = django_filters.CharFilter(lookup_type='icontains', name='translations__label')
 
     class Meta:
         model = Label

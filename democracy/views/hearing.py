1b6ef514dccc54d1b13ecedd2f83f1795566d232
Rikuoja <riku.oja@kapsi.fi>
2017-05-31 15:28:04 +0300

    Add distinct to title filter for titles in several languages

--- democracy/views/hearing.py
+++ democracy/views/hearing.py
@@ -29,7 +29,7 @@ from .utils import NestedPKRelatedField, filter_by_hearing_visible
 class HearingFilter(django_filters.FilterSet):
     open_at_lte = django_filters.IsoDateTimeFilter(name='open_at', lookup_type='lte')
     open_at_gt = django_filters.IsoDateTimeFilter(name='open_at', lookup_type='gt')
-    title = django_filters.CharFilter(lookup_type='icontains', name='translations__title')
+    title = django_filters.CharFilter(lookup_type='icontains', name='translations__title', distinct=True)
     label = django_filters.Filter(name='labels__id', lookup_type='in', distinct=True,
                                   widget=django_filters.widgets.CSVWidget)
 

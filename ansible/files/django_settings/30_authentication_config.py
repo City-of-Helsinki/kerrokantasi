000d0cbdaa7daccfa4218f377e084c3d6ffb26a2
Ville Koivunen <ville.koivunen@hel.fi>
2017-11-17 15:23:33 +0200

    Remove old settings files

--- ansible/files/django_settings/30_authentication_config.py
+++
@@ -1,5 +0,0 @@
-JWT_AUTH = {
-    'JWT_PAYLOAD_GET_USER_ID_HANDLER': 'helusers.jwt.get_user_id_from_payload_handler',
-    'JWT_SECRET_KEY': 'AfnAuF4Jt1EeiXWnJFQjeOiKE3PAKGqLyV9GB4HTvMOta5Cy4ywQng2e4QAw6UFLbDr8mfVCTZIAh08fmuwlpoegEDbs9vjH',
-    'JWT_AUDIENCE': 'vqjbAfTI8g6ZtAEXQ2xh87TMJm5qCWlddzK9YaA0'
-}

43aa3ed6ad6329b989dad57acd4afe95d350d694
sane <santtu.suominen@haltu.fi>
2017-11-20 10:48:16 +0200

    test for contactperson, label and sectiontype admins

---
+++ democracy/tests/test_sectiontype_admin.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import pytest
+from django.core.urlresolvers import reverse
+
+
+@pytest.mark.django_db
+def test_sectiontype_admin_renders(admin_client):
+    url = reverse('admin:democracy_sectiontype_changelist')
+    response = admin_client.get(url)
+    assert response.status_code == 200
+
+    url = reverse('admin:democracy_sectiontype_add')
+    response = admin_client.get(url)
+    assert response.status_code == 200
+

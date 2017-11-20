9ddd5a57e54baf1576a7a28cf3cd3565c43aabdb
sane <santtu.suominen@haltu.fi>
2017-11-20 10:48:16 +0200

    test for contactperson, label and sectiontype admins

---
+++ democracy/tests/test_contactperson_admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import pytest
+from django.core.urlresolvers import reverse
+
+
+@pytest.mark.django_db
+def test_contactperson_admin_renders(admin_client, contact_person):
+    url = reverse('admin:democracy_contactperson_changelist')
+    response = admin_client.get(url)
+    assert response.status_code == 200
+
+    url = reverse('admin:democracy_contactperson_change', args=[contact_person.id])
+    response = admin_client.get(url)
+    assert response.status_code == 200
---
+++ democracy/tests/test_label_admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import pytest
+from django.core.urlresolvers import reverse
+
+
+@pytest.mark.django_db
+def test_label_admin_renders(admin_client, default_label):
+    url = reverse('admin:democracy_label_changelist')
+    response = admin_client.get(url)
+    assert response.status_code == 200
+
+    url = reverse('admin:democracy_label_change', args=[default_label.id])
+    response = admin_client.get(url)
+    assert response.status_code == 200

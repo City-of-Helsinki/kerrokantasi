d215f6ae09ee126986d827381e5058be038d5d63
Rikuoja <riku.oja@kapsi.fi>
2017-12-15 15:42:31 +0200

    Change base.py database default to postgis for travis compatibility

--- kerrokantasi/settings/base.py
+++ kerrokantasi/settings/base.py
@@ -66,8 +66,8 @@ WSGI_APPLICATION = 'kerrokantasi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'kerrokantasi',
     }
 }
 

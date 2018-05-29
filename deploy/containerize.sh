19e012b32dd4676e61c8173567b0e6468d1aa358
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-05-29 16:55:30 +0300

    Create Docker container and deploy to hub

---
+++ deploy/containerize.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PYTHON_VERSION" != "3.6" ]; then
+    echo "Only deploy on production Python build"
+    exit 0
+fi
+
+export ORGANIZATION="helsinki"
+
+export IMAGE="kerrokantasi"
+
+export REPO="$ORGANIZATION/$IMAGE"
+
+export COMMIT=${TRAVIS_COMMIT::7}
+
+export BRANCH=${TRAVIS_BRANCH//\//_}
+
+echo "Building image"
+docker build -t $IMAGE .
+
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
+echo "Tagging branch " "$TRAVIS_BRANCH"
+docker tag $IMAGE "$REPO:$COMMIT"
+docker tag "$REPO:$COMMIT" "$REPO:$BRANCH"
+docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER"
+docker push "$REPO:$COMMIT"
+docker push "$REPO:travis-$TRAVIS_BUILD_NUMBER"
+docker push "$REPO:$BRANCH"

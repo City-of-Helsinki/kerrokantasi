891f11458f37c6164f3ae2ada1ab3c1a9b4b0d6b
ezkat <50319957+ezkat@users.noreply.github.com>
2020-02-11 13:24:12 +0200

    Fix template syntax error
    * Update to django-helusers 0.5.6

--- kerrokantasi/urls.py
+++ kerrokantasi/urls.py
@@ -2,17 +2,19 @@ from nested_admin import urls as nested_admin_urls
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.cache import never_cache
 from democracy import urls_v1
 from democracy.views.upload import browse, upload
 
+from helusers.admin_site import admin
+
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('helusers.urls')),
     url('', include('social_django.urls', namespace='social')),
     url(r'^v1/', include(urls_v1)),
     url(r'^nested_admin/', include(nested_admin_urls)),
-    url(r'^admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^upload/', staff_member_required(upload), name='ckeditor_upload'),
     url(r'^browse/', never_cache(staff_member_required(browse)), name='ckeditor_browse'),

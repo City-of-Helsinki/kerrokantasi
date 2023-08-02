60d7675f8dbece17f689a487759dbccbd58d6daf
Juha Louhiranta <juha.louhiranta@anders.fi>
2023-08-02 13:18:25 +0300

    Fix deprecated management command arguments

--- democracy/management/commands/democracy_mock_populate.py
+++ democracy/management/commands/democracy_mock_populate.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.core.management.base import BaseCommand
-from optparse import make_option
 
 from democracy.factories.hearing import HearingFactory, LabelFactory
 from democracy.factories.user import UserFactory
@@ -10,13 +9,9 @@ from democracy.models import Hearing, Label
 
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option(
-            "--nuke",
-            dest="nuke",
-            action="store_true",
-        ),
-    )
+
+    def add_arguments(self, parser):
+        parser.add_argument("--nuke", dest="nuke", action="store_true")
 
     def handle(self, *args, **options):
         if options.pop("nuke", False):

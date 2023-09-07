8a7dc468e96b5efda30a65998fd9a3e986a7526c
Juha Louhiranta <juha.louhiranta@anders.fi>
2023-09-07 12:04:53 +0300

    Contact person endpoint can only be accessed by admins
    Refs KER-239, KER-202

--- democracy/views/contact_person.py
+++ democracy/views/contact_person.py
@@ -1,8 +1,15 @@
-from rest_framework import mixins, permissions, response, serializers, status, viewsets
+from rest_framework import mixins, permissions, serializers, viewsets
 
 from democracy.models import ContactPerson, Organization
 from democracy.pagination import DefaultLimitPagination
 from democracy.views.utils import TranslatableSerializer
+
+
+class ContactPersonPermission(permissions.BasePermission):
+    message = "User without organization cannot access contact persons."
+
+    def has_permission(self, request, view):
+        return bool(hasattr(request.user, "get_default_organization") and request.user.get_default_organization())
 
 
 class ContactPersonSerializer(serializers.ModelSerializer, TranslatableSerializer):
@@ -15,26 +22,12 @@ class ContactPersonSerializer(serializers.ModelSerializer, TranslatableSerialize
 
     def create(self, validated_data):
         if not validated_data["organization"]:
-            validated_data["organization"] = self.context['request'].user.get_default_organization()
+            validated_data["organization"] = self.context["request"].user.get_default_organization()
         return super().create(validated_data)
 
 
 class ContactPersonViewSet(viewsets.ReadOnlyModelViewSet, mixins.CreateModelMixin, mixins.UpdateModelMixin):
     serializer_class = ContactPersonSerializer
     queryset = ContactPerson.objects.select_related("organization").order_by("name")
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = [permissions.IsAuthenticated & ContactPersonPermission]
     pagination_class = DefaultLimitPagination
-
-    def create(self, request, **kwargs):
-        if not request.user or not request.user.get_default_organization():
-            return response.Response(
-                {'status': 'User without organization cannot POST contact persons.'}, status=status.HTTP_403_FORBIDDEN
-            )
-        return super().create(request, **kwargs)
-
-    def update(self, request, pk=None, partial=False):
-        if not request.user or not request.user.get_default_organization():
-            return response.Response(
-                {'status': 'User without organization cannot PUT contact persons.'}, status=status.HTTP_403_FORBIDDEN
-            )
-        return super().update(request, pk=pk, partial=partial)

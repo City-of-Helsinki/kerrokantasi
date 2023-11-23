726ec55faf3ce6054b2e67016f020e4708209d78
Matti Eiden <matti.eiden@anders.com>
2023-11-23 17:45:31 +0200

    refactor: update django-helusers and simplify oidc implementation
    django-helusers 0.9.0 is marked as a requirement for keycloak migration.
    
    Simplifies the derived ApiTokenAuthentication class.
    To reduce cognitive load, prefer to create new names for new classes
     rather than renaming common base classes via import statements.
    
    Ref: KER-106

--- kerrokantasi/oidc.py
+++ kerrokantasi/oidc.py
@@ -1,25 +1,25 @@
 from django.conf import settings
-from helusers.oidc import ApiTokenAuthentication as HelApiTokenAuth
+from helusers.oidc import ApiTokenAuthentication
 
 
-class ApiTokenAuthentication(HelApiTokenAuth):
-    def __init__(self, *args, **kwargs):
-        super(ApiTokenAuthentication, self).__init__(*args, **kwargs)
-
+class StrongApiTokenAuthentication(ApiTokenAuthentication):
     def authenticate(self, request):
-        jwt_value = self.get_jwt_value(request)
-        if jwt_value is None:
+        result = super().authenticate(request)
+
+        if result is None:
             return None
 
-        payload = self.decode_jwt(jwt_value)
-        user, auth = super(ApiTokenAuthentication, self).authenticate(request)
+        user, auth = result
 
         # amr (Authentication Methods References) should contain the used auth
         # provider name e.g. suomifi
-        if payload.get("amr") in settings.STRONG_AUTH_PROVIDERS:
+        old_has_strong_auth = user.has_strong_auth
+        if auth.data.get("amr") in settings.STRONG_AUTH_PROVIDERS:
             user.has_strong_auth = True
         else:
             user.has_strong_auth = False
 
-        user.save()
-        return (user, auth)
+        if old_has_strong_auth != user.has_strong_auth:
+            user.save(update_fields=["has_strong_auth"])
+
+        return user, auth

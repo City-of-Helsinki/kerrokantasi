0800b8a0ae451772071aeb8e8db4ec8b1f7df51b
Matti Eiden <matti.eiden@anders.com>
2023-12-04 12:29:28 +0200

    to-be-squashed: simplify based on comments

--- kerrokantasi/oidc.py
+++ kerrokantasi/oidc.py
@@ -4,22 +4,18 @@ from helusers.oidc import ApiTokenAuthentication
 
 class StrongApiTokenAuthentication(ApiTokenAuthentication):
     def authenticate(self, request):
-        result = super().authenticate(request)
-
-        if result is None:
+        authentication_result = super().authenticate(request)
+        if authentication_result is None:
             return None
 
-        user, auth = result
+        user, auth = authentication_result
 
         # amr (Authentication Methods References) should contain the used auth
         # provider name e.g. suomifi
         old_has_strong_auth = user.has_strong_auth
-        if auth.data.get("amr") in settings.STRONG_AUTH_PROVIDERS:
-            user.has_strong_auth = True
-        else:
-            user.has_strong_auth = False
-
-        if old_has_strong_auth != user.has_strong_auth:
+        user.has_strong_auth = auth.data.get("amr") in settings.STRONG_AUTH_PROVIDERS
+        if user.has_strong_auth != old_has_strong_auth:
+            # Strong auth status has changed, update it
             user.save(update_fields=["has_strong_auth"])
 
         return user, auth

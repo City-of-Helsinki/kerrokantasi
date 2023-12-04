87b26e5a1507ecceaa17b197d6f9ed873d0b7b97
Matti Eiden <matti.eiden@anders.com>
2023-12-04 12:29:39 +0200

    to-be-squashed: add tests for StrongApiTokenAuthentication

---
+++ kerrokantasi/tests/test_auth.py
@@ -0,0 +1,38 @@
+from helusers._oidc_auth_impl import ApiTokenAuthentication
+from helusers.authz import UserAuthorization
+from unittest.mock import Mock, patch
+
+from kerrokantasi.oidc import StrongApiTokenAuthentication
+
+
+def test_api_token_authentication_none():
+    auth = StrongApiTokenAuthentication()
+    request = Mock()
+    with patch.object(ApiTokenAuthentication, "authenticate") as super_authenticate:
+        super_authenticate.return_value = None
+        assert auth.authenticate(request) is None
+
+
+def test_api_token_authentication_regular():
+    authentication = StrongApiTokenAuthentication()
+    user = Mock()
+    authorization = UserAuthorization(user, {"amr": "FOO"})
+    request = Mock()
+    with patch.object(ApiTokenAuthentication, "authenticate") as super_authenticate:
+        super_authenticate.return_value = (user, authorization)
+        result = authentication.authenticate(request)
+        assert result == (user, authorization)
+        assert user.has_strong_auth == False
+
+
+def test_api_token_authentication_strong(settings):
+    settings.STRONG_AUTH_PROVIDERS = ("FOO",)
+    authentication = StrongApiTokenAuthentication()
+    user = Mock()
+    authorization = UserAuthorization(user, {"amr": "FOO"})
+    request = Mock()
+    with patch.object(ApiTokenAuthentication, "authenticate") as super_authenticate:
+        super_authenticate.return_value = (user, authorization)
+        result = authentication.authenticate(request)
+        assert result == (user, authorization)
+        assert user.has_strong_auth == True

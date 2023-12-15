750e3767887a93b6cf56dabc298dfa2fdf9ea673
Daniel Prange <daniel.prange@anders.com>
2023-12-15 14:27:08 +0200

    ci: refactor entrypoint
    - use an entrypoint similar to other City of Helsinki projects, e.g. https://github.com/City-of-Helsinki/linkedevents/blob/main/docker/django/docker-entrypoint.sh
    - add APPLY_MIGRATIONS, COMPILE_TRANSLATIONS settings for toggling the corresponding operations on or off
    - delete obsolete files

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -2,71 +2,27 @@
 
 set -e
 
-TIMESTAMP_FORMAT="+%Y-%m-%d %H:%M:%S"
-
-function _log () {
-    echo "$(date "$TIMESTAMP_FORMAT")": "$RUN_ID": "$@"
-}
-
-function _log_boxed () {
-    _log ---------------------------------
-    _log "$@"
-    _log ---------------------------------
-}
-
-_log_boxed "Kerrokantasi container"
-
-if [[ "$1" = "help" ]]; then
-    _log "This is a container image for running Kerrokantasi API server"
-    _log ""
-    _log "By default a production ready server will be started using uWSGI"
-    _log "You will need to provide configuration using environment variables, see"
-    _log "config_dev.toml for the available variables. Especially important is the"
-    _log "database configuration using DATABASE_URL"
-    _log ""
-    _log "You will need to mount a volume at /srv/media for storing user uploaded"
-    _log "media. Otherwise they will be lost at container shutdown."
-    _log ""
-    _log "In addition to the production server, there are several task commands:"
-    _log "- start_django_development_server: runs Django development server at port 8000"
-    _log "- initial_data: runs some initial import tasks to populate the database"
-    _log "- migrate: runs Django migrations (manage.py migrate)"
-    _log "- test: runs Django tests (pytest)"
-    _log "- maintenance_tasks: run periodic maintenance tasks, with level as parameter"
-    _log "- <anything else>: run command in shell, verbatim"
-    _log ""
-    _log_boxed "This container will now exit, for your convenience"
-
-    exit 0
+if [[ "$WAIT_FOR_IT_HOSTS" ]]; then
+    deploy/wait-for-it.sh $WAIT_FOR_IT_HOSTS --timeout=30
 fi
 
-_log "Start with \`help\` for instructions"
+if [[ "$APPLY_MIGRATIONS" = "true" ]]; then
+    echo "Applying database migrations..."
+    ./manage.py migrate --noinput
+fi
 
-if [[ "$1" = "start_django_development_server" ]]; then
-    _log_boxed "Running development server"
-    exec deploy/dev_start.sh
-elif [[ "$1" = "maintenance_tasks" ]]; then
-    _log_boxed "Running scheduled data imports and maintenance"
-    deploy/run_maintenance.sh
-elif [[ "$1" = "initial_data" ]]; then
-    _log_boxed "Running initial imports to get test data"
-    deploy/run_maintenance.sh
-elif [[ "$1" = "migrate" ]]; then
-    _log_boxed "Running migrations"
-    ./manage.py migrate
-    _log_boxed "Updating language fields & installing templates"
-    deploy/init_application.sh
-elif [[ "$1" = "test" ]]; then
-    _log_boxed "Running flake8"
-    flake8 democracy
-    _log_boxed "Running tests"
-    pytest
-elif [[ -n "$*" ]]; then
-    _log_boxed "exec'n $*"
-    exec "$@"
+if [[ "$COMPILE_TRANSLATIONS" = "true" ]]; then
+    echo "Compiling translations..."
+    django-admin compilemessages
+fi
+
+# Allow running arbitrary commands instead of the servers
+if [[ -n "$*" ]]; then
+    "$@"
+elif [[ "$DEV_SERVER" = "true" ]]; then
+    echo "Starting development server"
+    exec python -Wd ./manage.py runserver 0.0.0.0:8000
 else
-    _log_boxed "Starting production server"
+    echo "Starting production server"
     exec uwsgi -y deploy/uwsgi.yml
 fi
-
-_log_boxed "Hauki entrypoint finished"
--- deploy/run_maintenance.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This file is for any management commands Kerrokantasi needs to run hourly, i.e. cronjobs
-
-set -e

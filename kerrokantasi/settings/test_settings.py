3a1dae87bfb549fe4a492641d1b2d1dbf6f52b6c
Matti Eiden <matti.eiden@anders.com>
2024-01-03 14:31:42 +0200

    chore: support test_settings for pytest
    Due to the way the regular settings are made, test_settings also relies
     on the use of globals() to update the settings.

---
+++ kerrokantasi/settings/test_settings.py
@@ -0,0 +1,8 @@
+from . import *  # noqa
+
+# Using globals() to avoid issues with type checking which most likely
+# can not follow the use of globals().update in the settings.__init__
+
+OIDC_API_TOKEN_AUTH = globals()["OIDC_API_TOKEN_AUTH"]
+OIDC_API_TOKEN_AUTH["API_AUTHORIZATION_FIELD"] = ["authorization.permissions.scopes"]
+OIDC_API_TOKEN_AUTH["ISSUER"] = ["http://test.local:8000/openid"]

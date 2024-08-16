0bcabab352729da6b3d1ed43b2c2fa2f06475bd4
Juha Louhiranta <juha.louhiranta@anders.fi>
2024-08-16 11:51:21 +0300

    chore: use the same variable name for a logger

--- democracy/models/project.py
+++ democracy/models/project.py
@@ -1,12 +1,9 @@
-import logging
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from parler.managers import TranslatableQuerySet
 from parler.models import TranslatableModel, TranslatedFields
 
 from democracy.models.base import ORDERING_HELP, BaseModelManager, StringIdBaseModel
-
-LOG = logging.getLogger(__name__)
 
 
 class Project(StringIdBaseModel, TranslatableModel):

642997658b56fa505eb3b80f9e83cc88d5a42721
Daniel Prange <daniel.prange@anders.com>
2024-10-24 15:34:14 +0300

    feat!: remove django-ckeditor and related views
    Refs: KER-396

--- kerrokantasi/urls.py
+++ kerrokantasi/urls.py
@@ -1,13 +1,10 @@
 from django.conf import settings
-from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import include, path
-from django.views.decorators.cache import never_cache
 from django.views.generic.base import RedirectView
 from helusers.admin_site import admin
 from nested_admin import urls as nested_admin_urls
 
 from democracy import urls_v1
-from democracy.views.upload import browse, upload
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,9 +12,6 @@ urlpatterns = [
     path("", include("social_django.urls", namespace="social")),
     path("v1/", include(urls_v1)),
     path("nested_admin/", include(nested_admin_urls)),
-    path("ckeditor/", include("ckeditor_uploader.urls")),
-    path("upload/", staff_member_required(upload), name="ckeditor_upload"),
-    path("browse/", never_cache(staff_member_required(browse)), name="ckeditor_browse"),
     path("gdpr-api/", include("helsinki_gdpr.urls")),
     path("", RedirectView.as_view(url="v1/")),
 ]

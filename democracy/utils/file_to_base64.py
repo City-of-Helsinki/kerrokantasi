10600538c61e492f214086662f2c7a804a311012
Mika Hietanen <m.hietanen84@gmail.com>
2026-08-22 09:20:22 +0300

    refactor: split file encoding expression
    Name intermediate values to keep the data URI readable.
    
    Refs: KER-641

--- democracy/utils/file_to_base64.py
+++ democracy/utils/file_to_base64.py
@@ -4,4 +4,6 @@ from typing import IO
 
 
 def file_to_base64(file: IO[bytes]) -> str:
-    return f"data:{mimetypes.guess_type(file.name)[0]};base64,{base64.b64encode(file.read()).decode('ascii')}"  # noqa: E501
+    mime_type = mimetypes.guess_type(file.name)[0]
+    encoded_file = base64.b64encode(file.read()).decode("ascii")
+    return f"data:{mime_type};base64,{encoded_file}"
